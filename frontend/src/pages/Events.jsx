a9d89c24d920ea1f22aa6b35cc71ab67b9d63ae0
unknown <mohamedsubhibouchi@gmail.com>
2019-03-19 23:24:28 +0100

    modal to create new event NOT FINISHED

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import './Events.css'
+import Modal from '../components/Modal/Modal'
+import Backdrop from '../components/Backdrop/Backdrop'
+
 
 export default class Events extends Component {
+
+  state = {
+    creating: false
+  }
+
+  startCreateEventHandler = () => {
+    this.setState({creating: true})
+  }
+
+  modalConfirmHandler = () => {
+    this.setState({creating:false})
+  }
+
+  modalCancelHandler = () => {
+    this.setState({creating: false})
+  }
   render() {
+    console.log(this.state.creating)
     return (
-      <div>
-        events
-      </div>
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && 
+          <Modal onCancel={this.modalCancelHandler}
+                  onConfirm={this.modalConfirmHandler}
+                  title="add event" canCancel canConfirm><p>content</p>
+          </Modal>
+        }
+        <div className="events-control">
+          <p>share events</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>create event</button>
+        </div>
+      </React.Fragment>
     )
   }
 }
