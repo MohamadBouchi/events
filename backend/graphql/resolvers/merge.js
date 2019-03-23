7b472c4ec2691c0fa2381be3a2ca2b2685ee9054
unknown <mohamedsubhibouchi@gmail.com>
2019-03-23 22:05:08 +0100

    install and use dataloader

--- backend/graphql/resolvers/merge.js
+++ backend/graphql/resolvers/merge.js
@@ -2,6 +2,17 @@
 const Event = require('../../models/events')
 const User = require('../../models/user')
 const { dateToString } = require('../../helpers/date')
+const DataLoader = require('dataloader')
+
+
+const eventLoader = new DataLoader((eventIds) => {
+    return events (eventIds)
+})
+
+
+const userLoader = new DataLoader((userIds) => {
+    return User.find({_id: {$in: userIds}})
+})
 
 
 const transformEvent = event => {
@@ -27,8 +38,8 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
     try {
-        const event = await Event.findById(eventId)
-        return transformEvent(event)
+        const event = await eventLoader.load(eventId.toString())
+        return event
     } catch (err) {
         throw err
     }
@@ -36,11 +47,11 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
     try{
-        const user = await User.findById(userId)
+        const user = await userLoader.load(userId.toString())
         return { 
                 ...user._doc
                 ,_id: user.id
-                ,createdEvents: events.bind(this, user._doc.createdEvents)
+                ,createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
             }
     }
     catch(err) {
@@ -62,6 +73,3 @@ const transformBooking = booking => {
 
 exports.transformEvent = transformEvent
 exports.transformBooking = transformBooking
-// exports.user = user
-//exports.events = events
-// exports.singleEvent = singleEvent
